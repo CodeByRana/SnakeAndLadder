1b6094fd2384b28313c857dc3cc2f97cc1443fa0
deepakphpweb <dipakrana2760@gmail.com>
2021-11-28 15:22:36 +0530

    Added : Create a snakeAndLadder Proram so took one variable first

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome To Snake and Ladder Program");
+
+        int startPosition = 0;
     }
 }
