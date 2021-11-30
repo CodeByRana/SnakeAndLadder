347f92eef1124b089a2edbe63d20429c94d9ed1a
deepakphpweb <dipakrana2760@gmail.com>
2021-12-01 00:24:18 +0530

    Added By Rana : Create a Snake And Ladder Program UseCase5

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,27 +3,34 @@ package com.bridgelabz;
 public class SnakeAndLadder {
 
     public static final int Start_Position = 0;
+    public static final int Winning_Position = 100;
     public static final int IS_SNAKE = 1;
     public static final int IS_LADDER = 2;
+    public static int currentPosition = 0;
 
     public static void main(String[] args) {
-        
-            int currentPostion = Start_Position;
+
+        while (currentPosition < Winning_Position) {
+
             int Dice_Number = (int) (Math.random() * 6 + 1);
             int option = (int) ((Math.random() * 10 % 3) + 1);
 
             if (option == IS_SNAKE) {
-                currentPostion -= Dice_Number;
-                if (currentPostion < 0) {
-                    currentPostion = Start_Position;
+                currentPosition -= Dice_Number;
+                if (currentPosition < 0) {
+                    currentPosition = Start_Position;
                 }
-                System.out.println("IS_SNAKE " + currentPostion);
+                System.out.println("IS_SNAKE : " + currentPosition);
             } else if (option == IS_LADDER) {
-                currentPostion += Dice_Number;
-                System.out.println("IS_LADDER " + currentPostion);
-
+                if (currentPosition + Dice_Number <= 100) {
+                    currentPosition += Dice_Number;
+                } else {
+                    System.out.println("No Play");
+                }
+                System.out.println("IS_LADDER : " + currentPosition);
             } else {
-                System.out.println("No Play " + currentPostion);
+                System.out.println("No Play");
             }
+        }
     }
 }
